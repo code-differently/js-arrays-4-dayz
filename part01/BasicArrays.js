d4eff16f26e4b680b4301a43f0ce4a4bd15103fa
Shelley McIntyre <73217296+SLMcIntyre@users.noreply.github.com>
2020-12-19 15:47:42 -0500

    completed BasicArrays test

--- part01/BasicArrays.js
+++ part01/BasicArrays.js
@@ -4,7 +4,7 @@ class BasicArrays{
      * @return the first element in the array
      */
     getFirstElement(stringArray) {
-        return null;
+           return stringArray[0];
     }
 
     /**
@@ -12,7 +12,7 @@ class BasicArrays{
      * @return the second element in the array
      */
     getSecondElement(stringArray) {
-        return null;
+        return stringArray[1];
     }
 
     /**
@@ -20,7 +20,11 @@ class BasicArrays{
      * @return stringArray with the elements in reverse order
      */
     reverse(stringArray) {
-        return null;
+        let newArray = [];
+         for(let i=stringArray.length - 1; i>=0; i--){
+            newArray.push(stringArray[i]);
+        }
+        return newArray;
     }
 
     /**
@@ -28,7 +32,16 @@ class BasicArrays{
      * @return String made up of the first character in each element of stringArray
      */
    getFirstLetterOfEachElement(stringArray) {
-        return null;
+        let newArray = [];
+        let firstElement = [];
+       
+       for (let i=0; i<=stringArray.length-1; i++){
+           newArray += stringArray[i][0];
+           
+       }
+       firstElement.push(newArray);
+
+        return (firstElement.join(""));
     }   
 }
 
