4bd3ee935fe596c722082c59ab9e86c47cf6bbe6
Tajanae Boone <taj@Tajanaes-MacBook-Air.local>
2020-12-20 18:39:15 -0500

    arrays

--- part01/BasicArrays.js
+++ part01/BasicArrays.js
@@ -4,7 +4,7 @@ class BasicArrays{
      * @return the first element in the array
      */
     getFirstElement(stringArray) {
-        return null;
+        return stringArray[0];
     }
 
     /**
@@ -12,24 +12,35 @@ class BasicArrays{
      * @return the second element in the array
      */
     getSecondElement(stringArray) {
-        return null;
+        return stringArray[1];
     }
 
     /**
      * @param stringArray an array of String objects
      * @return stringArray with the elements in reverse order
      */
-    reverse(stringArray) {
-        return null;
+        reverse(stringArray) {
+            const length = stringArray.length;
+            const halfLength = Math.floor(length/2);
+            for(let i = 0; i < halfLength; i++ ){
+                const rightNum = stringArray[length - 1 - i];
+                stringArray[length - 1 - i] = stringArray[i];
+                stringArray[i] = rightNum;
+        }
+            return stringArray;
     }
 
     /**
      * @param stringArray an array of String objects
      * @return String made up of the first character in each element of stringArray
      */
-   getFirstLetterOfEachElement(stringArray) {
-        return null;
-    }   
-}
+    getFirstLetterOfEachElement(stringArray) {
+        let result = "";
+        for (let i = 0; i < stringArray.length; i ++)
+            result += stringArray[i].charAt(0);
+        return result;
+     }
+
+    }
 
 module.exports = BasicArrays;
--- part02/Craze4Arrays.js
+++ part02/Craze4Arrays.js
@@ -6,17 +6,18 @@ class Craze4Arrays{
         @return an array with identical content excluding the specified `objectToRemove`
         Given an array of objects, name `objectArray`, and an object `objectToRemove`, return an array of objects with identical contents excluding `objectToRemove`
     */
-    removeValue(objectArray, objectToRemove) {
-        return null;
-    }
+
+   removeValue = (objectArray, objectToRemove) => objectArray.filter( (val) => val !== objectToRemove );
 
     /**
      * @param objectArray an array of any type of Object
      * @return the most frequently occurring object in the array
      * given an array of objects, named `objectArray` return the most frequently occuring object in the array
      */
-    getMostCommon(objectArray) {
-        return null;
+    getNumberOfOccurrences(objectArray, valueToEvaluate){
+        return objectArray.reduce((acc,elem) => {
+            return (valueToEvaluate === elem ? acc + 1 : acc)
+        },0);
     }
 
     /**
@@ -25,18 +26,20 @@ class Craze4Arrays{
      * given an array of objects, named `objectArray` return the least frequently occuring object in the array
      */
    getLeastCommon(objectArray) {
-        return null;
-    }
-
+    let result = objectArray.sort((a,b) => 
+    objectArray.filter(v => v === b).length - objectArray.filter(v => v === a).length).pop();
+            return result;
+}
     /**
      * @param objectArray      an array of any type of Object
      * @param objectArrayToAdd an array of Objects to add to the first argument
      * @return an array containing all elements in `objectArray` and `objectArrayToAdd`
      * given two arrays `objectArray` and `objectArrayToAdd`, return an array containing all elements in `objectArray` and `objectArrayToAdd`
      */
-    pmergeArrays(objectArray, objectArrayToAdd) {
-        return null;
-    }
+    mergeArrays(objectArray, objectArrayToAdd) {
+        let newArr = objectArray.concat(objectArrayToAdd);
+         return newArr;
+     }
 
 }
 
