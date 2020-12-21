797ca29963d8006684499fb2c7eda29249e17dc7
Shelley McIntyre <73217296+SLMcIntyre@users.noreply.github.com>
2020-12-21 02:19:04 -0500

    added completed solutions for CrazeArrays

--- part02/Craze4Arrays.js
+++ part02/Craze4Arrays.js
@@ -19,14 +19,12 @@ class Craze4Arrays{
         Given an array of objects, name `objectArray`, and an object `objectToRemove`, return an array of objects with identical contents excluding `objectToRemove`
     */
     removeValue(objectArray, objectToRemove) {
-        // let index = objectArray.indexOf(objectToRemove);
         for (let i=0; i<objectArray.length; i++){
         if (objectArray[i] === objectToRemove){
             objectArray.splice(i, 1);
             i--;
         }
        } 
-    //    let removed = objectArray.pop();
         return  objectArray;
     }
 
@@ -36,16 +34,38 @@ class Craze4Arrays{
      * given an array of objects, named `objectArray` return the most frequently occuring object in the array
      */
     getMostCommon(objectArray) {
-        return null;
-    }
+        let m = 0;
+        let mf = 1;
+        let mode;
+        for (let i=0; i<objectArray.length; i++){
+            for (let j = i; j<objectArray.length; j++){
+                if (objectArray[i] == objectArray[j])
+                m++;
+                if (mf<m){
+                    mf=m;
+                }
+            }
+            mode = mf;
+            m=0; 
+        }
+
+        return mode;
+        }
+       
+    
 
     /**
      * @param objectArray an array of any type of Object
      * @return the least frequently occurring object in the array
      * given an array of objects, named `objectArray` return the least frequently occuring object in the array
      */
-   getLeastCommon(objectArray) {
-        return null;
+   getLeastCommon(objectArray){
+       let leastCommon = objectArray.sort((a,b) =>
+            objectArray.filter(v => v===b).length
+          - objectArray.filter(v => v===a).length
+          ).pop();
+
+        return leastCommon;
     }
 
     /**
@@ -55,7 +75,8 @@ class Craze4Arrays{
      * given two arrays `objectArray` and `objectArrayToAdd`, return an array containing all elements in `objectArray` and `objectArrayToAdd`
      */
     mergeArrays(objectArray, objectArrayToAdd) {
-        return null;
+        let mergedArray = objectArray.concat(objectArrayToAdd);
+        return mergedArray;
     }
 
 }
